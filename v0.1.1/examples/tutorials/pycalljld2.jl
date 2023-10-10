22c82aa3d19d8c23fa53b6ea6cb2cf53cffa7e7d
Documenter.jl <documenter@juliadocs.github.io>
2023-10-10 22:16:25 +0000

    build based on 6a4c8f8

---
+++ v0.1.1/examples/tutorials/pycalljld2.jl
@@ -0,0 +1,25 @@
+ENV["PYTHON"] = ""
+using Pkg
+Pkg.build("PyCall")
+
+# Load the modules into the current context
+using
+    PyCall,     # for PyObjects
+    JLD2,       # for saving and loading
+    PyCallJLD2  # for telling JLD2 how to save and load PyObjects
+
+@pyimport sklearn.linear_model as lm
+
+m1 = lm.LinearRegression()
+
+m2 = lm.ARDRegression()
+
+model_file = "models.jld2"
+
+JLD2.save(model_file, "mods", [m1, m2])
+
+models = JLD2.load(model_file, "mods")
+
+rm(model_file)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
